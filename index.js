762d49bcf6bd8c4bb45282ab191ee6cdd99c07c0
rasel <russelroy15@gmail.com>
2021-10-26 04:07:26 +0600

    WIP on ema john node mongo server project

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const express = require("express");
+const { MongoClient } = require("mongodb");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+const uri =
+  "mongodb+srv://<username>:<password>@cluster0.rzfie.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.get("/", (req, res) => {
   res.send("Ema john server is Running");
